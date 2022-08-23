77aa3afc6d029bd7439b72837adaa935d3abe084
Bruno Cocco <brunoe.cocco@gmail.com>
2022-08-23 01:38:34 -0300

    primer commit

---
+++ Cuentas.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo " Hola Bienvenido a Dividir Cuentas"
+sleep 1
+
+echo "Cuantos es el monto a dividir?"
+read monto
+
+echo "Cuantos participantes son?"
+read cantUsuarios
+
+echo " Estoy calculando los montos ..."
+echo " solo tomara unos segundos"
+sleep 2
+
+echo "deben poner cada uno  $monto / $cantUsuarios "
+sleep 1
+echo " gil de goma."
+
+
