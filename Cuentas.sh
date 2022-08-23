54cee2ffb1070f90b0b7f4634d0392de0ace8111
Bruno Cocco <brunoe.cocco@gmail.com>
2022-08-23 01:45:44 -0300

    agergando funcionalidades

--- Cuentas.sh
+++ Cuentas.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-echo " Hola Bienvenido a Dividir Cuentas"
+echo "  Hola Bienvenido a Dividir Cuentas "
 sleep 1
 
 echo "Cuantos es el monto a dividir?"
 read monto
 
-echo "Cuantos participantes son?"
+echo "Cuantos participantes son ?"
 read cantUsuarios
 
-echo " Estoy calculando los montos ..."
-echo " solo tomara unos segundos"
+echo " ** Estoy calculando los montos ** "
+echo "Solo tomara unos segundos"
 sleep 2
 
-echo "deben poner cada uno  $monto / $cantUsuarios "
+echo "Deben poner cada uno: $monto / $cantUsuarios "
+echo "Hace la division..."
 sleep 1
-echo " gil de goma."
+echo "- gil de goma."
 
+echo "xD"
 
