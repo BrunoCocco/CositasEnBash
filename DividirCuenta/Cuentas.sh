16ea0bbbbc0b52494f8c2dce8c4dd7a16df30aff
Bruno Cocco <brunoe.cocco@gmail.com>
2022-08-23 04:08:20 -0300

    agregando cosas al DividirCuents

--- DividirCuenta/Cuentas.sh
+++ DividirCuenta/Cuentas.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-echo "  Hola Bienvenido a Dividir Cuentas "
+echo " ** Hola Bienvenido a Dividir Cuentas **"
 sleep 1
-
-echo "Cuantos es el monto a dividir?"
+echo ""
+echo " ** Cuantos es el monto a dividir? ** "
 read monto
-
 echo "Cuantos participantes son ?"
 read cantUsuarios
-
+echo ""
 echo " ** Estoy calculando los montos ** "
-echo "Solo tomara unos segundos"
+echo " ** Solo tomara unos segundos** "
 sleep 2
-
-echo "Deben poner cada uno: $monto / $cantUsuarios "
-echo "Hace la division..."
-sleep 1
-echo "- gil de goma."
-
-echo "xD"
-
+let total=($monto/$cantUsuarios)
+echo "Cada uno debe saldar: $" $total".-"
