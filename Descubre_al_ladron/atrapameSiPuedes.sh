95bb8bf3c01fd6fda7da968b0b7d3350314487e4
Bruno Cocco <brunoe.cocco@gmail.com>
2022-08-24 22:32:32 -0300

    comenzmos ASP

---
+++ Descubre_al_ladron/atrapameSiPuedes.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#TODO: En este juego encontraremos al ladron.
+
+# 
+# 
+echo "
+    
+██▀▄─██─▄─▄─█▄─▄▄▀██▀▄─██▄─▄▄─██▀▄─██▄─▀█▀─▄█▄─▄▄─███─▄▄▄▄█▄─▄███▄─▄▄─█▄─██─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█
+██─▀─████─████─▄─▄██─▀─███─▄▄▄██─▀─███─█▄█─███─▄█▀███▄▄▄▄─██─█████─▄▄▄██─██─███─▄█▀██─██─██─▄█▀█▄▄▄▄─█
+▀▄▄▀▄▄▀▀▄▄▄▀▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▀▀▄▄▄▄▄▀▄▄▄▀▀▀▄▄▄▀▀▀▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀"
+
+sleep 2
+echo "
+
+--- Suena el telefono y me apuro por responder
+"
+sleep 2
+clear
+echo " 
+       *** tuuu
+       *** tuuu
+       *** tuuu
+"
+echo "
+    Hola -
+    
+        - Hola Seriff, Soy su pequeño amigo, creo que puede hacerse la idea.
+        estoy por la ciudad, y quiero
+
+    No tengo idea quien eres, gracias por llamar -
+"
