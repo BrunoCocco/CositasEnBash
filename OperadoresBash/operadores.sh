44ccf68c321d5b0b6616cc819e8d59027f644881
Bruno Cocco <brunoe.cocco@gmail.com>
2022-09-22 16:34:32 -0300

    agregando practica de operadores relacionales

--- OperadoresBash/operadores.sh
+++ OperadoresBash/operadores.sh
@@ -5,4 +5,15 @@ numB=4;
 
 echo "operadores aritmeticos"
 echo "numero a=$numA y b=$numB"
-echo "sumar a + b =" $((numA + numB))
+
+sleep 1
+echo "sumar: a + b =" $((numA + numB))
+echo "restar: a - b =" $((numA - numB))
+echo "multiplicar: a * b =" $((numA * numB))
+echo "dividir a / b =" $((numA / numB))
+echo "resto a / b =" $((numA % numB))
+
+sleep 2
+echo "operadores Relacionales"
+echo "Numeros a=$numA y b=$numB"
+echo "A > B =" $((numA < numB))
