f73741531426cd19c48e87e383d782cb61622e83
Bruno Cocco <brunoe.cocco@gmail.com>
2022-09-23 00:57:55 -0300

    agregando sustitucion de comandos por variables

---
+++ OperadoresBash/sustComands/sustituir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#sustituir comandos por variables
+# "Bruno Cocco."
+#ejercicio 5
+
+ubicacionAct=`pwd`
+infoKernel=$(uname -a)
+
+echo "la ubi es :  "
+echo "$ubicacionAct "
+echo " el kernel es :" 
+echo " $infoKernel "
